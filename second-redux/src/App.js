5546693c20c529c3a60e586c5b01c958cbdb4177
Wojciech Sapiechowski <wojciech.sapiechowski@wearenurture.com>
2020-04-03 18:22:12 +0200

    - Initial with redux

---
+++ second-redux/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+      <div style={{textAlign: 'center'}}>
+        <p>Hejka <span role="img"></span>👋🏻</p>
+      </div>
+  );
+}
+
+export default App;
